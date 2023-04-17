14466c4d28923237311e3067fa5136a4d7d97b72
yoroih <roycristhopherqp@gmail.com>
2023-04-16 20:31:49 -0500

    feat: corrección

--- dibujar_cuadrados/script.js
+++ dibujar_cuadrados/script.js
@@ -11,12 +11,28 @@ const crearCuadrado = (idcanvas, color, linewidth, numero) => {
 
     for (let i = 0; i < numero*50; i+=50){
 
-        let a = i;
-        console.log(a);
-        dibujar.fillRect(i,0,tamañoX,tamañoY);
-        dibujar.strokeStyle = "black";
-        dibujar.strokeRect(i,0,tamañoX,tamañoY);    
+        while (i < figura.width){
+                console.log(i)
+                dibujar.fillRect(i,0,tamañoX,tamañoY);
+                dibujar.strokeStyle = "black";
+                dibujar.strokeRect(i,0,tamañoX,tamañoY);
+                i += 50;
+                console.log(i);
+        }
+
+
+        
+        // if (i < figura.width){
+
+        //     console.log(i)
+        //     dibujar.fillRect(i,0,tamañoX,tamañoY);
+        //     dibujar.strokeStyle = "black";
+        //     dibujar.strokeRect(i,0,tamañoX,tamañoY);
+        // } else if (i >= figura.with) {
+        //     i = 0;
+        //     console.log("toca ir a la siguiente linea")
+        // } 
+
     }
 
-    inicio++;
 }
