cdc14299aaa595f11c0004dab3a0507bd411a81a
yoroih <roycristhopherqp@gmail.com>
2023-04-17 18:16:50 -0500

    feat: modif function crearCuadrados

--- dibujar_cuadrados/script.js
+++ dibujar_cuadrados/script.js
@@ -1,6 +1,5 @@
-const crearCuadrado = (idcanvas, color, linewidth, numero) => {
-    let tamañoX = 100;
-    let tamañoY = 100;
+const crearCuadrado = (idcanvas, color, linewidth, numero, tamañoX = 100, tamañoY=100) => {
+    
     let cuadradosPintados = 0;
 
     let figura = document.getElementById(idcanvas);
@@ -11,15 +10,18 @@ const crearCuadrado = (idcanvas, color, linewidth, numero) => {
 
     for (let i = 0; i < numero; i++) {
         // Calcular coordenadas x e y del cuadrado actual
+
         let x = (cuadradosPintados % (figura.width / tamañoX)) * tamañoX;
         let y = Math.floor(cuadradosPintados / (figura.width / tamañoX)) * tamañoY;
     
+
         // Pintar cuadrado en las coordenadas calculadas
         dibujar.fillRect(x, y, tamañoX, tamañoY);
         dibujar.strokeStyle = "black";
         dibujar.strokeRect(x, y, tamañoX, tamañoY);
     
         cuadradosPintados++;
+
     }
 };
 
