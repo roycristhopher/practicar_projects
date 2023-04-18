4b5a48d0d8e1ad3473018e616570df6685e327ef
roycristhopher <yoroicode@gmail.com>
2023-04-18 01:36:59 -0500

    chore: anotations

--- 3_grafico_de_barras/README.md
+++ 3_grafico_de_barras/README.md
@@ -2,7 +2,8 @@
 
 ### Ejercicio donde puedes generar un gráfico de barras a partir de arrays con los datos del mismo tamaño.
 
-### El gráfico no tiene un limite, se pueden agregar tantos datos como se desee.
+### El gráfico no tiene un limite, se pueden agregar tantos datos como se desee, mientras no se sobrepase el alto del **canvas**
 
 >_Faltan agregar INPUTS para poder hacerlo todo desde la vista usuario_
+>_Hay un problema al repetir la función en otro canvas_
 
--- 3_grafico_de_barras/script.js
+++ 3_grafico_de_barras/script.js
@@ -1,7 +1,7 @@
 
 let posY = 0;
 
-const graficar = (canvaID, lista, colores,texto) => {
+const graficar = (canvaID, lista, colores,texto, titulo) => {
 
   for (let i=0; i<lista.length; i++){
     
@@ -9,16 +9,17 @@ const graficar = (canvaID, lista, colores,texto) => {
     let dibujar = figura.getContext("2d");
       
     dibujar.fillStyle = colores[i];
-    dibujar.fillRect(50,posY,50,lista[i]);
+    dibujar.fillRect(50,posY+25,50,lista[i]);
     dibujar.strokeStyle = "black";
-    dibujar.strokeRect(50,posY,50,lista[i]);
+    dibujar.strokeRect(50,posY+25,50,lista[i]);
 
     posY = posY+lista[i];
 
     dibujar.font="15px Georgia";
     dibujar.fillStyle="black";
-    dibujar.fillText(texto[i],0,posY);
-    dibujar.fillText(lista[i],110,posY);
+    dibujar.fillText(titulo,0,15);
+    dibujar.fillText(texto[i],0,posY+20);
+    dibujar.fillText(lista[i],110,posY+20);
 
   }
 
