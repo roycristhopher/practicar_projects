0d7c05dd86e5ce84543a7c4186833bad433826a1
yoroih <roycristhopherqp@gmail.com>
2023-04-19 21:07:43 -0500

    new proj

--- 6_ tiro_al_blanco/script.js
+++ 6_ tiro_al_blanco/script.js
@@ -1,16 +1,22 @@
 let figura = document.getElementById("cuadro")
 let dibujar = figura.getContext("2d");
 
+//variables de posición aleatoria para X y Y
+const generarAleatorio = () => {
+    let numeroRandom = Math.random();
+    return numeroRandom;
+}
+
 dibujar.fillStyle = "grey";
 dibujar.fillRect(0,0,figura.width,figura.height);
 
 const disparar = (evento) => {
-    let x = evento.pageX - figura.offsetLeft;
-    let y = evento.pageY - figura.offsetTop;
-    let d = new Date();
-    let hora = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    let a = evento.pageX - figura.offsetLeft;
+    let b = evento.pageY - figura.offsetTop;
+    // let d = new Date();
+    // let hora = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
     
-    alert(`la hora es ${hora} y las coordenadas son x=${x} y y=${y}`);
+    alert(`Las coordenadas son x=${a} y y=${b}`);
 }
 
 const drawCircle = (x=10,y=10,radio=10,color="red") => {
@@ -20,20 +26,26 @@ const drawCircle = (x=10,y=10,radio=10,color="red") => {
     dibujar.fill();
 }
 
-const valores = (color1,color2) => {
-
-    let diana = [40,30,20,10,5];
-    let color = [color1,color2,color1,color2,"red"];
-    let x = Math.round(Math.random()*(figura.width - (diana[0]*2))) + diana[0];
-    let y = Math.round(Math.random()*(figura.height - (diana[0]*2))) + diana[0];
-    for(let i = 0; i < diana.length; i++){
-
-        drawCircle(x,y,diana[i],color[i]);
-    }
+const cleanFigure = () => {
+    dibujar.clearRect(0,0,figura.width, figura.height);
 }
 
-valores("black", "white");
+const generarDiana = (color1,color2) => {
+    cleanFigure();
+    let num = generarAleatorio();
 
+    let diana = [40,30,20,10,3];
+    let color = [color1,color2,color1,color2,"red"];
+    let xAleatorio = Math.round(num*(figura.width - (diana[0]*2))) + diana[0];
+    let yAleatorio = Math.round(num*(figura.height - (diana[0]*2))) + diana[0];
+    for(let i = 0; i < diana.length; i++){
+        drawCircle(xAleatorio,yAleatorio,diana[i],color[i]);
+    }
+    return(num);
+}
 
+let x = generarDiana("red","white");
+console.log(x)
 
-
+//setInterval(()=>{generarDiana("red","white")},1000);
+//figura.onclick = disparar;
