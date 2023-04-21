a621a14b3af0b04bee1c2c79cfa5b792a2df5fa8
yoroih <roycristhopherqp@gmail.com>
2023-04-20 21:45:26 -0500

    chore: changes

--- 6_ tiro_al_blanco/script.js
+++ 6_ tiro_al_blanco/script.js
@@ -1,23 +1,8 @@
 let figura = document.getElementById("cuadro")
 let dibujar = figura.getContext("2d");
 
-//variables de posición aleatoria para X y Y
-const generarAleatorio = () => {
-    let numeroRandom = Math.random();
-    return numeroRandom;
-}
-
 dibujar.fillStyle = "grey";
 dibujar.fillRect(0,0,figura.width,figura.height);
-
-const disparar = (evento) => {
-    let a = evento.pageX - figura.offsetLeft;
-    let b = evento.pageY - figura.offsetTop;
-    // let d = new Date();
-    // let hora = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-    
-    alert(`Las coordenadas son x=${a} y y=${b}`);
-}
 
 const drawCircle = (x=10,y=10,radio=10,color="red") => {
     dibujar.fillStyle = color;
@@ -32,20 +17,25 @@ const cleanFigure = () => {
 
 const generarDiana = (color1,color2) => {
     cleanFigure();
-    let num = generarAleatorio();
 
-    let diana = [40,30,20,10,3];
+    let diana = [40,30,20,10,5];
     let color = [color1,color2,color1,color2,"red"];
-    let xAleatorio = Math.round(num*(figura.width - (diana[0]*2))) + diana[0];
-    let yAleatorio = Math.round(num*(figura.height - (diana[0]*2))) + diana[0];
+
+    let xAleatorio = Math.round(Math.random()*(figura.width - (diana[0]*2))) + diana[0];
+    let yAleatorio = Math.round(Math.random()*(figura.height - (diana[0]*2))) + diana[0];
     for(let i = 0; i < diana.length; i++){
         drawCircle(xAleatorio,yAleatorio,diana[i],color[i]);
     }
-    return(num);
+    console.log("dentro de la función: "+xAleatorio, yAleatorio)
+    return { x: xAleatorio, y: yAleatorio };
+
 }
 
-let x = generarDiana("red","white");
-console.log(x)
+const disparar = (evento) => {
+    let a = evento.pageX - figura.offsetLeft;
+    let b = evento.pageY - figura.offsetTop;
+}
 
-//setInterval(()=>{generarDiana("red","white")},1000);
+//setInterval(()=>{generarDiana("black","white")},1000);
+
 //figura.onclick = disparar;
